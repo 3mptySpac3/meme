b5bb9fbd206c3d75be71ab59a2cb284d68a2f69a
3mptySpac3 <thefantasticalproject@gmail.com>
2023-11-10 05:56:25 -0700

    icons/nav/toggle
    almost done nav... should be able to work on the bottom part

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
-
+import HamburgerIcon from './comps/hambIcon';
+import { Icon } from '@iconify/react';
 import React, { useState } from 'react';
 
 const HomePage = () => {
@@ -10,28 +11,31 @@ const HomePage = () => {
       <div id="Above Div" style={{ height: '5vh' }} className="w-full relative flex items-center justify-center">
         <button
           onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          className="absolute top-5 right-5 p-2 bg-[#EDF0DA] text-[#303633] px-2 py-1 rounded-md"
+          className="absolute top-5 right-5 p-2 py-1 rounded-md "
         >
-          Menu
+          <HamburgerIcon />
         </button>
         {/* Horizontal Dropdown Sidbar */}
-        {isSidebarOpen && (
-          <div  className=" w-8/12 bg-[#EDF0DA] mt-6 rounded-b-[15px]">
+        {isSidebarOpen&& (
+            <div className={`w-10/12 bg-[#EDF0DA] mt-16 rounded-lg transform transition-all duration-75 ease-in-out ${isSidebarOpen ? 'translate-y-0' : '-translate-y-full'}`}>
             <div>{/*  Dropdown Sidbar content */}
               <ul className="flex flex-row justify-between p-4">
-                <li className="flex-1 text-center">l1</li>
-                <li className="flex-1 text-center">l2</li>
-                <li className="flex-1 text-center">l3</li>
-                <li className="flex-1 text-center">l4</li>
+                <li className="flex-1 text-center" ><Icon icon="ph:brain-thin" color="black" className="inline-block mr-2" width="40" height="40" /></li>
+                <li className="flex-1 text-center"><Icon icon="circum:dumbbell" color="black" className="inline-block mr-2" width="40" height="40" /></li>
+                <li className="flex-1 text-center"><Icon icon="ph:baby-thin" color="black" className="inline-block mr-2" width="40" height="40" /></li>
+                <li className="flex-1 text-center"><Icon icon="ph:heart-thin" color="black" className="inline-block mr-2" width="40" height="40" /></li>
+                <li className="flex-1 text-center"><Icon icon="arcticons:funmax" color="black" className="inline-block mr-2" width="40" height="40" /></li>
+                <li className="flex-1 text-center"><Icon icon="healthicons:money-bag-outline" color="black" className="inline-block mr-2" width="40" height="40" /></li>
+                <li className="flex-1 text-center"><Icon icon="arcticons:hwmirror" color="black" className="inline-block mr-2" width="40" height="40" /></li>
               </ul>
             </div>
           </div>)}
-      </div>
+      </div> {/* End of above div */}
       
       <div id="Border Line" className="border-b-2 border-[#EDF0DA] w-full mt-16"></div> {/* Border Line seperating top from buttom */}
-      <div className="flex-1 w-full">
+      <div className="flex-1 w-full"> {/* Below Div */}
         <h1 className="text-white text-3xl">Below</h1>
-      </div>
+      </div> {/* End of below div */}
     </div>
   );
 };
