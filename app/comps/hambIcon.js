36eb825c32de6a7ce2c77e37c9fe72120cabdf55
3mptySpac3 <thefantasticalproject@gmail.com>
2023-11-13 21:31:19 -0700

    ..

--- app/comps/hambIcon.js
+++ app/comps/hambIcon.js
@@ -1,9 +1,9 @@
 const HamburgerIcon = () => {
   return (
-     <div className="group flex flex-col items-center justify-center w-10 h-10 rounded bg-[#EDF0DA] hover:bg-black text-white transition duration-1000 ">
-      <span className="block w-6 h-0.5 bg-black mb-1 group-hover:bg-white transition duration-1000"></span>
-      <span className="block w-6 h-0.5 bg-black mb-1 group-hover:bg-white transition duration-1000"></span>
-      <span className="block w-6 h-0.5 bg-black group-hover:bg-white transition duration-1000"></span>
+     <div className="group flex flex-col items-center justify-center w-10 h-10 rounded bg-[#EDF0DA] hover:bg-black text-white transition duration-500 ">
+      <span className="block w-6 h-0.5 bg-black mb-1 group-hover:bg-white transition duration-500"></span>
+      <span className="block w-6 h-0.5 bg-black mb-1 group-hover:bg-white transition duration-500"></span>
+      <span className="block w-6 h-0.5 bg-black group-hover:bg-white transition duration-500"></span>
     </div>
   );
 };
